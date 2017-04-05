aac18c01181f1a18d6cea78237f2b9fc476dda31
psolbach <p@psolbach.com>
2017-04-05 16:56:35 +0200

    begin gallery view

--- js/dpa-liveblog.js
+++ js/dpa-liveblog.js
@@ -8,11 +8,13 @@ require('angular-animate/angular-animate');
 require('angular-resource/angular-resource');
 
 // 3rd party
-require('iframe-resizer')
+require('iframe-resizer');
+require('ng-photoswipe');
 
 // Angular base theme
 angular.module('liveblog-embed', [
   'ngResource',
+  'ngPhotoswipe',
   'angular-cache'
   ])
 
